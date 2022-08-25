bd17286fae627f4c2f61f9255aef0536e825cedd
Nandhini2712 <nandhiniraj2712@gmail.com>
2022-08-25 11:37:57 +0530

    first commit

---
+++ client/src/pages/Auth/Aboutauth.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Aboutauth = () => {
+  return (
+    <div className='auth-container-1'>
+        <h1>Join the stack Overflow community</h1>
+        <p>Get unstuck - ask a questions</p>
+        <p>Unlock new privilenges like voting and commenting</p>
+        <p>Save your favorite tags, filters and jobs</p>
+        <p>Earn reputation and badges</p>
+        <p style={{fontSize:'13px',color:'#666767'}}>Collabrate and share knowledge with a private group for</p>
+        <p style={{fontSize:'13px',color:'#007ac6'}}>Get Stack Overflow for Teams free for up to 50 users</p>
+
+    </div>
+  )
+}
+
+export default Aboutauth
